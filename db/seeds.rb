555cb5f8b943b823abc8c09929deb1df31da4f60
kelseyjdickson <kelseymedved@gmail.com>
2021-08-30 21:17:14 -0400

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,8 @@ kristal = Female.create({
 erica = Female.create({
         
         name: 'Erica Joy Baker',
-        company: "Github",
-        job_title: 'Director of Engineering',
+        company: "DCCC",
+        job_title: 'Chief Technology Officer',
         contact_link: 'https://www.linkedin.com/in/ericajoy/',
         image: 'https://media-exp1.licdn.com/dms/image/C5603AQF0Z_tWZ1Islg/profile-displayphoto-shrink_800_800/0/1516235373484?e=1631750400&v=beta&t=N2fxbk_VJ9NoDaxRKrV1I3H8CFuwNV-4SIP_Pvc8y2w'
 
