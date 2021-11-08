2d53a9aaa0b779efa6329a294f1d7e9f5ae8fba2
kelseyjdickson <kelseymedved@gmail.com>
2021-11-08 16:25:19 -0500

    adding specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'blogs#index'
-  resources :blogs
-  resources :females
+  
+  resources :blogs , only: [:index,:show]
+  resources :females, only: [:index,:show, :create, :update, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
