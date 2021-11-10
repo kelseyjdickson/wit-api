39b61de95c29830d524f86253bcf628e6fa02684
kelseyjdickson <kelseymedved@gmail.com>
2021-11-10 12:00:22 -0500

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://wit-spotlight.herokuapp.com/'
+    origins 'https://cors-anywhere.wit-spotlight.herokuapp.com/'
 
     resource '*',
       headers: :any,
