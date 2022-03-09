84b813a5335b3bde9056d9de7b460f90f0ef2ba5
kelseyjdickson <kelseymedved@gmail.com>
2022-03-09 16:58:23 -0500

    updated photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ joy = Female.create ({
         company: "Algorithmic Justice League",
         job_title: 'Computer Scientist - Poet of Code',
         contact_link: 'https://www.linkedin.com/in/buolamwini/',
-        image: Rails.root.join('app', 'assets', 'images', 'joy.png'),
+        image: 'https://miro.medium.com/max/1000/1*lmOZ-NZ1usxBIfirVOV3EQ.png',
         women_text: 'Joy is a Computer Scientist and Digital Activist who founded Algorithmic Justice League. Joy’s Ted Talk on algorithmic bias has been viewed more than a million times. Joy has influenced large tech companies to put a hold on developing facial recognition once federal regulations pass. She has testified before congress citing risks embedded in AI algorithms while explaining how many communities are harmed or impacted by this technology. Joy’s recent documentary, Coded Bias, shows her investigation of widespread bias in Algorithms.',
         website: "https://www.poetofcode.com/"
 
@@ -37,7 +37,7 @@ erica = Female.create({
         company: "DCCC",
         job_title: 'Chief Technology Officer',
         contact_link: 'https://www.linkedin.com/in/ericajoy/',
-        image: 'https://media-exp1.licdn.com/dms/image/C5603AQF0Z_tWZ1Islg/profile-displayphoto-shrink_800_800/0/1516235373484?e=1643241600&v=beta&t=Lk-bS9GPX14ihNcCsQfqOJ5LUlppb5o8ie5CE90guj0',
+        image: 'https://miro.medium.com/max/1000/1*2VzpucAnyCdIqd4D-bwY2g.png',
         women_text: 'Erica is the CTO at DCCC. She has worked at Github,Google, Slack, Patreon, and Microsoft. She has also been on the Advisory Boards for Hack the Hood, Girl Develop It, the Code.org Diversity Council, The Barbie Global Advisory, and as a Tech Mentor for Black Girls Code. Erica has been in the industry long enough to see the ins and outs of this particularly white-male dominated industry. Erica has been a huge advocate for diversity and inclusion. She is one of the founding members of Project Include, which helps companies to incorporate three values: Inclusion, Comprehensiveness, and Accountability.',
         website: "https://www.ericabaker.com/"
       })
@@ -48,7 +48,7 @@ kimberly = Female.create({
           company: "Black Girls Code",
           job_title: 'Founder and CEO of Black Girls Code',
           contact_link: 'https://www.linkedin.com/in/kimberlybryant/',
-          image: 'https://media-exp1.licdn.com/dms/image/C5603AQF8s0RURIyiVg/profile-displayphoto-shrink_400_400/0/1525450649976?e=1637193600&v=beta&t=cn16HH5cbvlYIft0rwCbphZXKqdLQxybOSgL_PNDAOw',
+          image: 'https://miro.medium.com/max/1000/1*eKamAf9Vi0zpR9HTgmY4Aw.png',
           women_text:'Kimberly founded Black Girls Code in 2011, a San Francisco based non-profit, which teaches young and pre-teen girls basic programming skills and in-demand technology through workshops and after school programs. Kimberly was inspired to find a more inclusive environment after her daughter expressed interest in programming.
           Kimberly hopes this exposure to young girls, especially those in underrepresented communities, helps encourage them to remain active in the STEM field. African American women make up 3% of the tech industry workforce. Black Girls Code is on a mission to change those statistics and to increase awareness within the tech world. The goal is to teach 1 million African American girls to code by 2040.',
           website:""
@@ -60,7 +60,7 @@ reshma = Female.create({
         company: "Girls Who Code",
         job_title: 'Founder of Girls Who Code',
         contact_link: 'https://www.linkedin.com/in/reshma-saujani/',
-        image: 'https://media-exp1.licdn.com/dms/image/C4D03AQEu66656x4unA/profile-displayphoto-shrink_400_400/0/1621632282104?e=1637193600&v=beta&t=EUsT0ZUWUhWQWjA9zChtq_mvsiusekq9g0roiRzTqAQ',
+        image: 'https://miro.medium.com/max/1000/1*HSIaWiwqOoOUi8YCB_oT6g.png',
         women_text:'During her run for U.S. Congress in 2010, Reshma visited local schools and witnessed firsthand the gender gap in computer classes. This led her to start Girls Who Code, with a mission of empowering young girls to explore technology. Girls Who Code also works with legislation to close the gender gap in K-12 classrooms, recommending their Policy Agenda to increase overall girls participation in computer science. 300k young girls have been served by Girls Who Code through their immersion programs, after school clubs, and college loops. Girls Who Code is committed to helping close the gender gap and their research shows that this could become a reality for entry-level jobs in 2027.',
         website:"https://reshmasaujani.com/"
       })
@@ -87,7 +87,7 @@ catherine = Female.create({
     company:'Stitch Fix',
     job_title: 'Software Engineer - Artists Who Code',
     contact_link: 'https://www.linkedin.com/in/catherinericafortmccreary/',
-    image: 'https://media-exp1.licdn.com/dms/image/C4D03AQE2-xUyIyijQg/profile-displayphoto-shrink_200_200/0/1569865760735?e=1639612800&v=beta&t=5oDiSzbgSl7tVDSl_vmG8FrydcFVROs_Jf1uf0XNaf4',
+    image: 'https://miro.medium.com/max/1000/1*R_41f0irw12-W0IiL6tJrQ.png',
     women_text: "Catherine is Co-Founder of Artists Who Code, an organization that advocates for creative professionals with non-traditional backgrounds in tech. Catherine also works as a Software Engineer at Stitch Fix. Prior to her journey and advocacy in tech, Catherine was a Broadway performer, appearing in multiple Broadway shows and television appearances. Artists Who Code has been a great resource for creative professionals looking to pivot during the COVID-19 pandemic. They have helped 100+ creatives land their first role in tech during this difficult time.",
     website: ""
 })
@@ -96,7 +96,7 @@ naz = Female.create({
     company:'Netflix',
     job_title: 'Production Engineer - Career Coach',
     contact_link: 'https://www.linkedin.com/in/nazdelam/',
-    image: 'https://miro.medium.com/max/1000/1*JaVfI49QX1wk9pnr-qkQqw.png',
+    image: 'https://miro.medium.com/max/1000/1*d3X02I7sNNIF7cqhSUtInw.png',
     women_text: "Naz is a Computer Scientist and has worked as a Software Engineer, Engineering Manager and Software Architect. Naz is passionate about mentorship and helping others grow their skills and has worked with new engineers through Hours of Code, Built By Girls, and various other platforms. Naz continues to offer monthly mentoring and one-off sessions to help engineers strengthen their skillsets and grow their careers. Naz has given talks for career growth and engineering at events such as Women of Silicon Valley and Women in Tech and many other speaking engagements.",
     website: "http://naz.dev/"
 })
@@ -105,7 +105,7 @@ manon = Female.create({
     company:'Currently Job Seeking',
     job_title: 'Software Engineer - Artist',
     contact_link: 'https://www.linkedin.com/in/manon-sainton-7830b01ab/',
-    image: 'https://media-exp1.licdn.com/dms/image/C4E03AQHrouWw4QWN8Q/profile-displayphoto-shrink_400_400/0/1611784379011?e=1640217600&v=beta&t=EYctjode4NU3hotsm7nVQow1DRyiVy8dzXrDPltuVLc',
+    image: 'https://miro.medium.com/max/1000/1*sdGnBxoiG7hVlAI6YW3pyA.png',
     women_text: "Manon is a Software Engineer and an NYC-based artist who goes by Sleepyfoot?. Manon has a background in art, education and technology. After working closely with engineers and data scientists in a previous role, Manon began to think about how she could incorporate tech into her practice as a synesthetic interdisciplinary artist. In 2020, Manon attended Flatiron School’s Software Engineering program. Manon is passionate about access and imagination. Manon says,” What can we build for communities? As we gain more experience during our time as an engineer, what can we conceive through code? What amendments can we make or solutions can we find that will shatter broken realities? I love the intersection of ideas and imagination. I’m excited to see what the future will bring!” Manon’s goals include helping bring exposure and helping women of color enter the tech industry. Manon believes in having a community to lean on without fancy word speak, bringing true authenticity, and providing support/empathy for any insecurities that may arise.",
     website: ""
 })
@@ -114,7 +114,7 @@ sammi = Female.create({
     company:'SourceStrike',
     job_title: 'Software Engineer',
     contact_link: 'https://www.linkedin.com/in/samantha-flores-313024112/',
-    image: 'https://media-exp1.licdn.com/dms/image/C5603AQEhpvVMlO4Naw/profile-displayphoto-shrink_400_400/0/1609364301369?e=1640217600&v=beta&t=CRmR8bJcovma_vLsFAvF9kHxzYzjNyADh3hxkEUDPXE',
+    image: 'https://miro.medium.com/max/1000/1*baVWRbNi5S7os3mufHGeRQ.png',
     women_text: 'Sammi’s background is in microbiology with an emphasis in medical lab research and testing, but she found a new passion for development after attending a full-stack coding boot camp at the University of California, Irvine in 2019. Since then she has experience working as a full-stack engineer at a fast-paced online start-up visualizing data in graphical formats, overhauling old and worn out features to reduce technical debt, and working closely with a small team to increase overall knowledge. Her career goals include becoming the type of mentor she always wanted and/or was lucky enough to have; constantly learning and passing that knowledge along to others, and never forgetting that it’s all “just code” and every problem is approachable. Personally, she hopes to become a better runner/run a 10k by the end of the year, get better at her hobby of making resin D&D/TTRPG dice, and to travel to every MLB stadium.',
     website: ""
 })
@@ -141,7 +141,7 @@ fariha = Female.create({
     company:'The Cohort Collective',
     job_title: 'Technical Writer + Career Coach',
     contact_link: 'https://www.linkedin.com/in/farihatsm/',
-    image: 'https://media-exp1.licdn.com/dms/image/C5603AQGM0DY58Npmcg/profile-displayphoto-shrink_200_200/0/1632453262188?e=1637798400&v=beta&t=TqYD0LsHc8Qo2Eqf7l47TEIn5uxw-pEN_tprGBoy6ck',
+    image: 'https://miro.medium.com/max/1152/1*dPCpwWM9eqh-rbsiPR3bRA.png',
     women_text:'Fariha is a powerhouse in the world of tech. She entered the tech space spontaneously after implementing a trial and error approach in her career development. Eventually, she was recruited for a role in sustainable tech, which gives her the ability to be an engineer in the pursuit of a zero-emission future. Accessibility for marginalized communities is something Fariha wishes could be different about the tech world. She says,” The barrier of entry for these communities is too high, ranging from unrealistic qualifications required for open roles to systemic oppression present across big tech culture. It shouldn’t be a barrier, it should be an open floodgate. Talent is out there, talent is diverse and talent deserves a fair chance to enter tech.” Fariha is proud of launching a career coaching firm, The Cohort Collective. The main goal is to embolden the Gen Y + Z career, equipping young people with the mindset and skillset to drive their careers forward. One of Fariha’s future goals is merging her personal and professional life and prioritizing a work-life balance with an emphasis on mental and physical health.',
     website: "http://www.farihatsm.carrd.co/"
 })
@@ -150,7 +150,7 @@ juliana = Female.create({
     company:'Remix at Via',
     job_title: 'Software Engineer',
     contact_link: 'https://www.linkedin.com/in/julianaalmeida78/',
-    image: 'https://media-exp1.licdn.com/dms/image/C5603AQEjxulYoXRLxw/profile-displayphoto-shrink_400_400/0/1561679294556?e=1642636800&v=beta&t=-7pz0nhmvrkvH1UJj8gtR4WLNypbmEk2SlRBa2SOwpw',
+    image: 'https://miro.medium.com/max/1152/1*XjyfpUOI853eVJtgny2u0g.png',
     women_text:'Ten years ago, Juliana moved to the United States from her hometown São Paulo, Brazil. Juliana currently works as a Software Engineer at Remix. On top of her full-time role, she is regularly posting helpful resources, new roles and apprenticeships on LinkedIn for others seeking opportunities. She is also continuing to grow her skills by taking a Python class at Foothill College. Juliana entered the tech world landing a contract role at Facebook as a content moderator. While the position was anything but easy, it exposed her to her first classes in coding. As her contract neared completion she looked for other opportunities and came across Techtonica, a Software Engineering bootcamp specifically for womxn and non-binary folx. She applied and was accepted to the bootcamp, and six months later she started interning at Remix. Initially one of the reasons Juliana wanted to get into tech was because she wanted to be a part of positive change in the industry. She acknowledges that it’s an ambitious task but cherishes the small wins. Some of the issues she highlights are lack of diversity, especially in tech leadership, and work-life balance. ”Work-life balance seems to be a rare thing in tech, and that may be one of the reasons some people can’t break in, especially mothers.” Proud moments in her career include completing the Techtonica bootcamp, in which she built an app with the help of more experienced engineers, and building and delivering a real project with clients during her internship. Anytime she has a new project at work she uses her growth mindset to view challenges as opportunities to grow and learn and celebrate once she’s able to ship it.',
     website:""
 })
